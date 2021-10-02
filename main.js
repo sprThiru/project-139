88435e7cd8ecc95619ea9a7b4aaf716bda414a87
sprThiru <87995087+sprThiru@users.noreply.github.com>
2021-10-02 10:15:21 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function setup()
+{
+canvas = createCanvas(550,450);
+canvas.center();
+
+video = createCapture(VIDEO);
+video.size(550, 450);
+video.hide();
+}
+function draw()
+{
+  background(0); 
+}
